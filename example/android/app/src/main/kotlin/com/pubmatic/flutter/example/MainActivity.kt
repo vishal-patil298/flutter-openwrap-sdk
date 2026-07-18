87b04842fe90104056af8f292a5fc75c21842a5b
Vishal Patil <vishal.patil@pubmatic.com>
2026-07-18 17:40:14 +0530

    2nd commit

--- example/android/app/src/main/kotlin/com/pubmatic/flutter/example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.pubmatic.sdk.flutter_openwrap_sdk_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-} 
--- example/lib/screens/screens.dart
+++
@@ -1,5 +0,0 @@
-export 'banner/openwrap_banner_screen.dart';
-export 'interstitial/openwrap_interstitial_screen.dart';
-export 'mrec_display/openwrap_mrec_display_screen.dart';
-export 'rewarded_ad/openwrap_rewarded_ad_screen.dart';
-export 'video_interstitial/openwrap_video_interstitial_screen.dart';
